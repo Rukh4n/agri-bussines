c6913845c652fd8e30a2e9768e7eebed896a684e
unknown <nurrukhans@gmail.com>
2024-04-24 20:21:52 +0700

    customiz layout userArticles

--- resources/js/Components/Users/Footer.jsx
+++ resources/js/Components/Users/Footer.jsx
@@ -3,24 +3,24 @@ import React from 'react'
 const Footer = () => {
     return (
 
-        <footer class="bg-white dark:bg-gray-900">
+        <footer class="bg-white dark:bg-slate-800">
             <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
                 <div class="md:flex md:justify-between">
                     <div class="mb-6 md:mb-0">
-                        <a href="https://flowbite.com/" class="flex items-center">
+                        <a href="#" class="flex items-center">
                             <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 me-3" alt="FlowBite Logo" />
-                            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+                            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">disoftwa</span>
                         </a>
                     </div>
                     <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
                         <div>
-                            <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Resources</h2>
+                            <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Contacs</h2>
                             <ul class="text-gray-500 dark:text-gray-400 font-medium">
                                 <li class="mb-4">
-                                    <a href="https://flowbite.com/" class="hover:underline">Flowbite</a>
+                                    <a href="#" class="hover:underline">Email</a>
                                 </li>
                                 <li>
-                                    <a href="https://tailwindcss.com/" class="hover:underline">Tailwind CSS</a>
+                                    <a href="#" class="hover:underline">WhatsApp</a>
                                 </li>
                             </ul>
                         </div>
@@ -28,10 +28,10 @@ const Footer = () => {
                             <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Follow us</h2>
                             <ul class="text-gray-500 dark:text-gray-400 font-medium">
                                 <li class="mb-4">
-                                    <a href="https://github.com/themesberg/flowbite" class="hover:underline ">Github</a>
+                                    <a href="https://github.com/Rukh4n" class="hover:underline ">Github</a>
                                 </li>
                                 <li>
-                                    <a href="https://discord.gg/4eeurUVvTy" class="hover:underline">Discord</a>
+                                    <a href="#" class="hover:underline">Instagram</a>
                                 </li>
                             </ul>
                         </div>
--- resources/js/Pages/Article/userArticles.jsx
+++ resources/js/Pages/Article/userArticles.jsx
@@ -6,24 +6,25 @@ const UserArticles = ({ articles, categories }) => {
     return (
         <>
             <NavBar />
-            <div className="grid grid-cols-4 gap-4">
+            <div className="grid mx-10 bg-black">
                 {articles.map((article, index) => {
                     const categoryId = article.category_id;
                     const categoryName = categories[categoryId]?.name;
                     const categorySlug = categories[categoryId]?.slug;
+
                     return (
-                        <div key={index}>
+                        <div key={index} className="mb-4 p-4 rounded-lg"> 
                             <a href={`/blog/${article.slug}`}>
-                                <h1>{article.title}</h1>
+                                <h1 className="text-white text-lg">{article.title}</h1>
                             </a>
                             <a href={`/blog_kategori/${categorySlug}`}>
-                                <p>{categoryName}</p>
+                                <p className="text-white text-xs">{categoryName}</p>
                             </a>
                         </div>
                     );
                 })}
             </div>
-            <Footer/>
+            <Footer />
         </>
     );
 };
